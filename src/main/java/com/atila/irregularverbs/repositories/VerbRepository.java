dfaa772ff6f82f0c699dc50e1719e22b42d34f9d
Atila Silva <atila__7@hotmail.com>
2022-11-07 21:35:30 -0300

    VerbRepository

--- src/main/java/com/atila/irregularverbs/repositories/VerbRepository.java
+++ src/main/java/com/atila/irregularverbs/repositories/VerbRepository.java
@@ -1,2 +1,9 @@
-package com.atila.irregularverbs.repositories;public interface VerbRepository {
+package com.atila.irregularverbs.repositories;
+
+import com.atila.irregularverbs.entities.Verb;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerbRepository extends JpaRepository<Verb, Long> {
 }
