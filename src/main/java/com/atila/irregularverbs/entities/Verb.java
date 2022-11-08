8ba8c2b51f6da25bbe8bc1e771e145fc01e34ed1
Atila Silva <atila__7@hotmail.com>
2022-11-07 21:30:42 -0300

    Verb Entity with JPA mapping

---
+++ src/main/java/com/atila/irregularverbs/entities/Verb.java
@@ -0,0 +1,65 @@
+package com.atila.irregularverbs.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_VERB")
+public class Verb implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String infinitive;
+
+    @Column(nullable = false)
+    private String simplePast;
+
+    @Column(nullable = false)
+    private String pastParticiple;
+
+    public Verb() {
+    }
+
+    public Verb(Long id, String infinitive, String simplePast, String pastParticiple) {
+        this.id = id;
+        this.infinitive = infinitive;
+        this.simplePast = simplePast;
+        this.pastParticiple = pastParticiple;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInfinitive() {
+        return infinitive;
+    }
+
+    public void setInfinitive(String infinitive) {
+        this.infinitive = infinitive;
+    }
+
+    public String getSimplePast() {
+        return simplePast;
+    }
+
+    public void setSimplePast(String simplePast) {
+        this.simplePast = simplePast;
+    }
+
+    public String getPastParticiple() {
+        return pastParticiple;
+    }
+
+    public void setPastParticiple(String pastParticiple) {
+        this.pastParticiple = pastParticiple;
+    }
+}
